eab9e7097c30b3c9ce01352b13a797002339f7dd
tothstevee <tothistvan2000@gmail.com>
2025-07-16 09:24:13 +0200

    Update page.tsx

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -2,11 +2,9 @@
 
 import Header from "@/components/header";
 import Hero from "@/components/hero";
-import HowItWorks from "@/components/how-it-works";
 import Features from "@/components/features";
 import Testimonials from "@/components/testimonails";
 import CallToAction from "@/components/call-to-action";
-import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import SignUp from "@/components/signup";
 
